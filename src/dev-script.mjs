ce934381631c01e99b5ec4f197633245f0f05622
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-02-27 12:40:01 +0100

    normal transfer

--- src/dev-script.mjs
+++ src/dev-script.mjs
@@ -9,8 +9,49 @@ const run = async () => {
 
   getTokenBalance(tokenContract, accounts[0], accounts[0])
     .then((balance) => {
-      console.log(balance);
+      console.log("Account 0 has balance " + balance);
     });
+
+  getTokenBalance(tokenContract, accounts[1], accounts[1])
+    .then((balance) => {
+      console.log("Account 1 has balance " + balance);
+    });
+
+  getTokenBalance(tokenContract, accounts[2], accounts[2])
+    .then((balance) => {
+      console.log("Account 2 has balance " + balance);
+    });
+
+  await tokenContract.methods.transfer(accounts[2], 500)
+    .send({
+      from: accounts[1],
+      gas: 80000000
+    })
+    .then(() => {
+      console.log("500 tokens transferred from account 1 to account 2")
+    });
+
+  getTokenBalance(tokenContract, accounts[0], accounts[0])
+    .then((balance) => {
+      console.log("Account 0 has balance " + balance);
+    });
+
+  getTokenBalance(tokenContract, accounts[1], accounts[1])
+    .then((balance) => {
+      console.log("Account 1 has balance " + balance);
+    });
+
+  getTokenBalance(tokenContract, accounts[2], accounts[2])
+    .then((balance) => {
+      console.log("Account 2 has balance " + balance);
+    });
+
+
+
+
+
+
+
 };
 
 run();
