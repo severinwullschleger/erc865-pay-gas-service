47f06d403b9b4ecfd1b67bfeb452d8b8e90d3c69
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-03-10 12:35:54 +0100

    working web3js recover

--- src/dev-script.mjs
+++ src/dev-script.mjs
@@ -52,15 +52,16 @@ const run = async () => {
   let input = web3.eth.abi.encodeParameters(
     ['bytes4','address','address','uint256','uint256','uint256'],
     // ['0x15420b71', '0x2E535479c6865E46A688D4C845E28613c6925fEe', accounts[2], '500', '5', '0']);
-    ['0x15420b71', tokenContract.options.address, accounts[2], '500', '5', '0']);
+    ['0x15420b71',
+      tokenContract.options.address,
+      accounts[2], '500', '5', '0']);
   console.log(input);
 
   let toSignHexString = web3.utils.keccak256(input);
   console.log(toSignHexString);
-  let toSignByteArray = web3.utils.hexToBytes(toSignHexString);
-
-  let message = await web3.eth.accounts.sign(toSignByteArray, "3d63b5b61cc9636a143f4d2c56a9609eb459bc2f8f168e448b65f218893fef9f");
-  console.log(toSignByteArray === message.message);
+  let message = await web3.eth.accounts.sign(toSignHexString,
+  "0x3d63b5b61cc9636a143f4d2c56a9609eb459bc2f8f168e448b65f218893fef9f");
+  console.log(web3.eth.accounts.recover(toSignHexString, message.signature));
 
   let eventSig;
   let hashedTx;
@@ -76,15 +77,13 @@ const run = async () => {
       eventSig = receipt.events.Signature.returnValues._signature;
       hashedTx = receipt.events.Signature.returnValues.hashedTx;
       from = receipt.events.Signature.returnValues.from;
-      console.log(from);
     });
 
   console.log(eventSig === message.signature);
   console.log(hashedTx === toSignHexString);
   console.log(from === accounts[1]);
-
-
-
+  console.log(from);
+  console.log(accounts[1]);
 };
 
 run();
