5c69e303822ef0ea0fe7eb761dc71d8d4dbc18e9
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-03-17 15:54:56 +0100

    working transferred pre signed example

--- src/dev-script.mjs
+++ src/dev-script.mjs
@@ -47,6 +47,7 @@ const run = async () => {
       console.log("Account 2 has balance " + balance);
     });
 
+  let nonce = 4;
   // transferPreSignedHashing from Utils.sol
   // function transferPreSignedHashing(address _token, address _to, uint256 _value, uint256 _fee, uint256 _nonce)
   //   return keccak256(abi.encode(bytes4(0x15420b71), _token, _to, _value, _fee, _nonce));
@@ -57,7 +58,7 @@ const run = async () => {
       accounts[2],
       '500',
       '5',
-      '0']);
+      nonce.toString()]);
   console.log(input);
 
   let toSignHexString = web3.utils.keccak256(input);
@@ -75,14 +76,30 @@ const run = async () => {
     accounts[2],
     500,
     5,
-    0
+    nonce
   )
     .send({
       from: accounts[0],
       gas: 80000000
     })
     .then((receipt) => {
-      console.log("transferPreSigned call successful");
+      console.log("500 tokens transferred from account 1 to account 2\n" +
+        "Transaction sent by account 0: Fee of 5 tokens transferred from account 1 to account 0");
+    });
+
+  await getTokenBalance(tokenContract, accounts[0], accounts[0])
+    .then((balance) => {
+      console.log("Account 0 has balance " + balance);
+    });
+
+  await getTokenBalance(tokenContract, accounts[1], accounts[1])
+    .then((balance) => {
+      console.log("Account 1 has balance " + balance);
+    });
+
+  await getTokenBalance(tokenContract, accounts[2], accounts[2])
+    .then((balance) => {
+      console.log("Account 2 has balance " + balance);
     });
 };
 
