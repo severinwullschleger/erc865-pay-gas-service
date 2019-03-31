aba3b0ac671ac8639be98cf43660d4ba7f13c468
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-03-31 13:30:39 +0200

    changing transfer method

--- src/backend/services/transfer.mjs
+++ src/backend/services/transfer.mjs
@@ -1,10 +1,14 @@
 import {tokenContract} from "../../helpers/get-deployed-smart-contracts.mjs";
 import getEthereumAccounts from "../../helpers/get-ethereum-accounts.mjs";
 import web3 from "../../helpers/web3Instance.mjs";
+import {getTokenBalance} from "../../smartcontracts/methods/token-balances.mjs";
 
-let accounts = getEthereumAccounts(web3);
 
-export const sendTransferPreSignedTransaction = (transactionObject) => {
+
+export const sendTransferPreSignedTransaction = async (transactionObject) => {
+
+  let accounts = await getEthereumAccounts(web3);
+
   return tokenContract.methods.transferPreSigned(
     transactionObject.signature,
     transactionObject.from,
@@ -14,7 +18,26 @@ export const sendTransferPreSignedTransaction = (transactionObject) => {
     transactionObject.nonce.toString()
   )
     .send({
-      from: accounts[0],
+      from: "0x5c59065f0486Af304B7E1A4243905527A35E0DB5",
       gas: 80000000
     })
+    .then(async (receipt) => {
+      console.log("500 tokens transferred from account 1 to account 2\n" +
+        "Transaction sent by account 0: Fee of 5 tokens transferred from account 1 to account 0");
+
+      await getTokenBalance(tokenContract, accounts[0], accounts[0])
+        .then((balance) => {
+          console.log("Account 0 has balance " + balance);
+        });
+
+      await getTokenBalance(tokenContract, accounts[1], accounts[1])
+        .then((balance) => {
+          console.log("Account 1 has balance " + balance);
+        });
+
+      await getTokenBalance(tokenContract, accounts[2], accounts[2])
+        .then((balance) => {
+          console.log("Account 2 has balance " + balance);
+        });
+    });
 };
