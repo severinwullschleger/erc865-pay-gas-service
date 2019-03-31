b0950affd5afd915e56bb8a685de5ab6a7e118e8
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-03-31 16:57:43 +0200

    extracted transfer

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,151 +1,17 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { __THIRD } from "./helpers/colors";
-import Icon from "./views/icons/Icon";
-import {InputField} from "./views/design-components/Inputs.js";
-import Button from "./views/design-components/Button.js";
+import Transfer from "./views/Transfer.jsx";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-
-const RelativeContainer = styled.div`
-  position: relative;
-  margin-top: 70px;
-`;
-
-const Title = styled.h1`
-  font-family: "Indie Flower", cursive;
-  font-size: 38px;
-  margin-right: 4px;
-`;
-
-const Absolute = styled.div`
-  position: absolute;
-  bottom: -30px;
-  right: -50px;
-  display: flex;
-  align-items: center;
-`;
-
-const FormContainer = styled.div`
-
-`;
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-`;
-
-const RowCentered = styled(Row)`
-  align-items: center;    //vertical  alignment
-`;
-
-const LeftComponent = styled.div`
-  width: 70px;
-  margin-right: 10px;
-`;
-
-const AmountInput = styled(InputField)`
-  width: 150px;
-  margin-right: 10px;
-`;
-
-const AddressInputField = styled(InputField)`
-  width: 370px  ;
-`;
-
-const AmountContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 5px;
-`;
-
-const Fee = styled.span`
-  font-style: italic;
-  margin-left: 5px;
-  margin-right: 5px;
-`;
-
-const Padded = styled.div`
-  padding-left: 8px;
-`;
-
-const PKInputField = styled(InputField)`
-  width: 540px;
-`;
-
-const PrivateKeyInfo = styled.div`
-  font-style: italic;
-  margin-left: 10px;
-  font-size: 10px;
-  line-height: 1.6;
-  margin-top: 3px;
-`;
-
-const RowMultiLines = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-`;
-
 class App extends Component {
   render() {
     return (
       <Container>
-        <RelativeContainer>
-          <Absolute>
-            <Title>Transfer</Title>
-          </Absolute>
-        </RelativeContainer>
-        <FormContainer>
-          <RowCentered>
-              <AmountInput
-                placeholder={"Amount"}/>
-            DOS tokens
-          </RowCentered>
-          <RowCentered>
-            <LeftComponent>
-            From:
-            </LeftComponent>
-            <AddressInputField
-              placeholder={"From Address"}/>
-          </RowCentered>
-          <RowCentered>
-            <LeftComponent>
-            To:
-          </LeftComponent>
-            <AddressInputField
-              placeholder={"To Address"}/>
-          </RowCentered>
-          <Row>
-            pay transaction cost with{' '}
-            <AmountContainer>
-              <div>
-                <Fee>5</Fee> DOS
-              </div>
-              <Padded>
-                {'≈'}<Fee>0.20</Fee> ETH
-              </Padded>
-              <Padded>
-                {'≈'}<Fee>0.20</Fee> USD
-              </Padded>
-            </AmountContainer>
-          </Row>
-          <RowMultiLines>
-            <PKInputField
-              placeholder={"Private key of the from address"}/>
-              <PrivateKeyInfo>
-                Your private key is only used to sign the entered transation data. It is neither stored nor send somewhere.
-              </PrivateKeyInfo>
-          </RowMultiLines>
-          <Button>
-            Send
-          </Button>
-        </FormContainer>
+        <Transfer/>
       </Container>
     );
   }
---
+++ src/frontend/views/Transfer.jsx
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import {InputField} from "./design-components/Inputs.js";
+import Button from "./design-components/Button.js";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-family: "Indie Flower", cursive;
+  font-size: 38px;
+`;
+
+const FormContainer = styled.div`
+
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 20px;
+`;
+
+const RowCentered = styled(Row)`
+  align-items: center;    //vertical  alignment
+`;
+
+const TitleRow = styled(RowCentered)`
+  margin-top: 20px;
+`;
+
+const LeftComponent = styled.div`
+  width: 70px;
+  margin-right: 10px;
+`;
+
+const AmountInput = styled(InputField)`
+  width: 150px;
+  margin-right: 10px;
+`;
+
+const AddressInputField = styled(InputField)`
+  width: 370px  ;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 5px;
+`;
+
+const Fee = styled.span`
+  font-style: italic;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+
+const Padded = styled.div`
+  padding-left: 8px;
+`;
+
+const PKInputField = styled(InputField)`
+  width: 540px;
+`;
+
+const PrivateKeyInfo = styled.div`
+  font-style: italic;
+  margin-left: 10px;
+  font-size: 10px;
+  line-height: 1.6;
+  margin-top: 3px;
+`;
+
+const RowMultiLines = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+class Transfer extends Component {
+  render() {
+    return (
+      <Container>
+        <TitleRow>
+            <Title>Transfer tokens</Title>
+        </TitleRow>
+        <FormContainer>
+          <RowCentered>
+            <AmountInput
+              placeholder={"Amount"}/>
+            DOS tokens
+          </RowCentered>
+          <RowCentered>
+            <LeftComponent>
+              From:
+            </LeftComponent>
+            <AddressInputField
+              placeholder={"From Address"}/>
+          </RowCentered>
+          <RowCentered>
+            <LeftComponent>
+              To:
+            </LeftComponent>
+            <AddressInputField
+              placeholder={"To Address"}/>
+          </RowCentered>
+          <Row>
+            pay transaction cost with{' '}
+            <AmountContainer>
+              <div>
+                <Fee>5</Fee> DOS
+              </div>
+              <Padded>
+                {'≈'}<Fee>0.20</Fee> ETH
+              </Padded>
+              <Padded>
+                {'≈'}<Fee>0.20</Fee> USD
+              </Padded>
+            </AmountContainer>
+          </Row>
+          <RowMultiLines>
+            <PKInputField
+              placeholder={"Private key of the from address"}/>
+            <PrivateKeyInfo>
+              Your private key is only used to sign the entered transation data. It is neither stored nor send somewhere.
+            </PrivateKeyInfo>
+          </RowMultiLines>
+          <Button>
+            Send
+          </Button>
+        </FormContainer>
+      </Container>
+    );
+  }
+}
+
+export default Transfer;
