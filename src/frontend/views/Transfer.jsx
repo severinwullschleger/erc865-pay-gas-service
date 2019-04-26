bbcdc3b3356cf5d8818ddcfdccb5b50ece3b3a70
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-04-26 19:55:56 +0200

    changed context, added validating address and isServiceContract router

--- src/frontend/views/Transfer.jsx
+++ src/frontend/views/Transfer.jsx
@@ -5,6 +5,7 @@ import Button from "./design-components/Button.js";
 import secp256k1 from "secp256k1";
 import {Web3Context} from '../contexts/Web3Context';
 import {getDomain} from "../../helpers/getDomain.mjs";
+import {withRouter} from 'react-router-dom';
 
 const Container = styled.div`
   display: flex;
@@ -75,6 +76,9 @@ const RowMultiLines = styled.div`
 
 const WideButton = styled(Button)`
   margin-top: 35px;
+  ${props => !props.disabled ? '' : `
+    opacity: 0.4
+  `}
 `;
 
 class Transfer extends Component {
@@ -84,8 +88,11 @@ class Transfer extends Component {
       tokenAddress: null,
       signature: null,
       from: null,
+      isFromValid: null,
       to: null,
+      isToValid: null,
       value: null,
+      isValueValid: null,
       fee: null,
       nonce: null,
       privateKey: null
@@ -99,8 +106,11 @@ class Transfer extends Component {
       // testing purposes
       ,
       value: 400,
+      isValueValid: true,
       from: '0x9ea02Ac11419806aB9d5A512c7d79AC422cB36F7',
+      isFromValid: true,
       to: '0xB5227F13682873884a8C394A4a7AcDf369199Dc5',
+      isToValid: true,
       privateKey: '3d63b5b61cc9636a143f4d2c56a9609eb459bc2f8f168e448b65f218893fef9f'
     })
   }
@@ -109,22 +119,38 @@ class Transfer extends Component {
     this.setState({[stateKey]: e.target.value})
   }
 
-  signTransactionData(web3Context) {
+  validateAddress(stateKey, e) {
+    if (this.context.web3.utils.isAddress(e.target.value)) {
+      this.setState({[stateKey]: true});
+    } else {
+      this.setState({[stateKey]: false});
+    }
+  }
+
+  isServiceContractAddress(e) {
+    //TODO access context and more than one service contract
+
+    if (this.context.serviceContract && this.context.serviceContract.options && this.context.serviceContract.options.address && e.target.value)
+      return this.context.serviceContract.options.address === e.target.value;
+    return false;
+  }
+
+  signTransactionData() {
     let nonce = Date.now();
     // transferPreSignedHashing from Utils.sol
     // function transferPreSignedHashing(address _token, address _to, uint256 _value, uint256 _fee, uint256 _nonce)
     //   return keccak256(abi.encode(bytes4(0x15420b71), _token, _to, _value, _fee, _nonce));
-    let input = web3Context.web3.eth.abi.encodeParameters(
+    let input = this.context.web3.eth.abi.encodeParameters(
       ['bytes4', 'address', 'address', 'uint256', 'uint256', 'uint256'],
       ['0x15420b71',
-        web3Context.tokenContract.options.address,
+        this.context.tokenContract.options.address,
         this.state.to,
         this.state.value.toString(),
         this.state.fee.toString(),
         nonce.toString()]);
     console.log(input);
 
-    let inputHash = web3Context.web3.utils.keccak256(input);
+    let inputHash = this.context.web3.utils.keccak256(input);
     let privateKey;
     if (this.state.privateKey.substring(0, 2) === "0x")
       privateKey = this.state.privateKey.substring(2);
@@ -144,7 +170,7 @@ class Transfer extends Component {
     this.setState({
       signature: signatureInHex,
       nonce,
-      tokenAddress: web3Context.tokenContract.options.address
+      tokenAddress: this.context.tokenContract.options.address
     });
   }
 
@@ -153,7 +179,7 @@ class Transfer extends Component {
     let transactionObject = this.state;
     delete transactionObject.privateKey;
 
-    console.log ("sending this object: ", transactionObject);
+    console.log("sending this object: ", transactionObject);
 
     fetch(`${getDomain()}/api/transfer`, {
       method: 'POST',
@@ -166,91 +192,97 @@ class Transfer extends Component {
       .then(response => {
         console.log(response);
       })
-      .catch( err => {
+      .catch(err => {
         console.log("something went wrong: ", err)
       });
   }
 
   render() {
     return (
-      <Web3Context.Consumer>
-        {web3Context => {
-          return (
-            <Container>
-              <FormContainer>
-                <RowCentered>
-                  <LeftComponent>
-                    <AmountInput
-                      placeholder={"Amount"}
-                      value={this.state.value}
-                      onChange={e => this.handleInput('value', e)}
-                    />
-                  </LeftComponent>
-                  DOS tokens
-                </RowCentered>
-                <RowCentered>
-                  <LeftComponent>
-                    From:
-                  </LeftComponent>
-                  <AddressInputField
-                    placeholder={"From Address"}
-                    value={this.state.from}
-                    onChange={e => this.handleInput('from', e)}
-                  />
-                </RowCentered>
-                <RowCentered>
-                  <LeftComponent>
-                    To:
-                  </LeftComponent>
-                  <AddressInputField
-                    placeholder={"To Address"}
-                    value={this.state.to}
-                    onChange={e => this.handleInput('to', e)}
-                  />
-                </RowCentered>
-                <Row>
-                  <LeftComponent>
-                    Transaction costs:
-                  </LeftComponent>
-                  <AmountContainer>
-                    <div>
-                      <Fee>
-                        {this.state.fee}
-                      </Fee> DOS
-                    </div>
-                    <Padded>
-                      {'≈'}<Fee>0.20</Fee> ETH
-                    </Padded>
-                    <Padded>
-                      {'≈'}<Fee>0.20</Fee> USD
-                    </Padded>
-                  </AmountContainer>
-                </Row>
-                <RowMultiLines>
-                  <PKInputField
-                    placeholder={"Private key of the from address"}
-                    value={this.state.privateKey}
-                    onChange={e => this.handleInput('privateKey', e)}
-                  />
-                  <PrivateKeyInfo>
-                    Your private key is only used to sign the entered transation data. It is neither stored nor send
-                    somewhere.
-                  </PrivateKeyInfo>
-                </RowMultiLines>
-                <WideButton
-                  onClick={async () => {
-                    await this.signTransactionData(web3Context);
-                    this.sendSignedTransaction();
-                  }}>
-                  Send
-                </WideButton>
-              </FormContainer>
-            </Container>
-          );
-        }}
-      </Web3Context.Consumer>
+      <Container>
+        <FormContainer>
+          <RowCentered>
+            <LeftComponent>
+              <AmountInput
+                placeholder={"Amount"}
+                value={this.state.value}
+                onChange={e => this.handleInput('value', e)}
+              />
+            </LeftComponent>
+            DOS tokens
+          </RowCentered>
+          <RowCentered>
+            <LeftComponent>
+              From:
+            </LeftComponent>
+            <AddressInputField
+              placeholder={"From Address"}
+              value={this.state.from}
+              onChange={e => {
+                this.handleInput('from', e);
+                this.validateAddress('isFromValid', e);
+              }}
+            />
+          </RowCentered>
+          <RowCentered>
+            <LeftComponent>
+              To:
+            </LeftComponent>
+            <AddressInputField
+              placeholder={"To Address"}
+              value={this.state.to}
+              onChange={e => {
+                this.handleInput('to', e);
+                this.validateAddress('isToValid', e);
+                if (this.isServiceContractAddress(e))
+                  this.props.history.push(`/transferAndCall`);
+              }}
+            />
+          </RowCentered>
+          <Row>
+            <LeftComponent>
+              Transaction costs:
+            </LeftComponent>
+            <AmountContainer>
+              <div>
+                <Fee>
+                  {this.state.fee}
+                </Fee> DOS
+              </div>
+              <Padded>
+                {'≈'}<Fee>0.20</Fee> ETH
+              </Padded>
+              <Padded>
+                {'≈'}<Fee>0.20</Fee> USD
+              </Padded>
+            </AmountContainer>
+          </Row>
+          <RowMultiLines>
+            <PKInputField
+              placeholder={"Private key of the from address"}
+              value={this.state.privateKey}
+              onChange={e => this.handleInput('privateKey', e)}
+            />
+            <PrivateKeyInfo>
+              Your private key is only used to sign the entered transation data. It is neither stored nor send
+              somewhere.
+            </PrivateKeyInfo>
+          </RowMultiLines>
+          <WideButton
+            disabled={!(this.state.isValueValid && this.state.isToValid && this.state.isFromValid)}
+            onClick={async () => {
+              console.log(this.state.isToValid)
+              await this.signTransactionData();
+              this.sendSignedTransaction();
+            }}>
+            Send
+          </WideButton>
+        </FormContainer>
+      </Container>
     );
   }
 }
 
-export default Transfer;
+Transfer.contextType = Web3Context;
+
+export default withRouter(Transfer);
