5285aee5d0f3ab9803161f0c5e9b4f8e6645e861
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-04-26 19:53:25 +0200

    moved config

--- src/backend/services/transfer.mjs
+++ src/backend/services/transfer.mjs
@@ -1,10 +1,8 @@
 import {tokenContract} from "../../helpers/get-deployed-smart-contracts.mjs";
-import getEthereumAccounts from "../../helpers/get-ethereum-accounts.mjs";
-import web3 from "../../helpers/web3Instance.mjs";
 import Web3PromiEvent from "web3-core-promievent";
 import {saveTransaction, updateTransaction} from "../db/transactions-services.mjs";
 import {TRANSACTION_STATUS} from "../db/transaction-states.mjs";
-import config from "../../../config.json"
+import config from "../../config.json"
 
 
 export const sendTransferPreSignedTransaction = async (transactionObject) => {
