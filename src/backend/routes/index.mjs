74a11f1777d1b689d621ecfeff8a66a4392adce2
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-05-12 11:13:35 +0200

    added transfer and call route

--- src/backend/routes/index.mjs
+++ src/backend/routes/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import {requestHandler} from "./requestHandler.mjs";
 import {sendTransferPreSignedTransaction} from "../services/transfer.mjs";
+import {sendTransferAndCallPreSignedTransaction} from "../services/transferAndCall.mjs";
 
 const router = express.Router();
 
@@ -8,6 +9,14 @@ router.post(
   '/transfer',
   requestHandler(async req => {
     return sendTransferPreSignedTransaction(req.body);
+  })
+);
+
+
+router.post(
+  '/transferAndCall',
+  requestHandler(async req => {
+    return sendTransferAndCallPreSignedTransaction(req.body);
   })
 );
 
