10f9e1a0813a48e66d747436eb3c4bd73ace018e
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-09-27 14:29:35 +0200

    error handling depl script

--- src/smartcontracts/deployment/deployment-script.mjs
+++ src/smartcontracts/deployment/deployment-script.mjs
@@ -3,18 +3,20 @@ import {finishMinting, mintTokens, setAdmins} from "../methods/token-minting.mjs
 
 const run = async () => {
   const [tokenContract, serviceContract] = await deployContractsAndSaveAddressesAndABIs();
+
   await mintTokens(tokenContract)
     .then(() =>{
       console.log('The DOS tokens have been minted.');
-    });
+    }).catch(error => console.error(error));
+
   await setAdmins(tokenContract)
     .then(() =>{
       console.log('Admins set.');
-    });
+    }).catch(error => console.error(error));
   await finishMinting(tokenContract)
     .then(() => {
       console.log('The minting has been finished.');
-    });
+    }).catch(error => console.error(error));
   process.exit();
 };
 
