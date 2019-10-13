6069abefcb364f0c63ef459bae96df613c61b5a5
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-10-13 10:48:10 +0200

    moved css

--- src/frontend/views/design-components/Notification/Toast.js
+++ src/frontend/views/design-components/Notification/Toast.js
@@ -1,6 +1,6 @@
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../Notification.css';
+import './Notification.css';
 import {css} from 'glamor';
 import {
   __ALERT_ERROR,
