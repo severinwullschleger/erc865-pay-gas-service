b4d7aac5979fab8187fb3dabc0ad374885193fa8
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-10-13 21:38:49 +0200

    qr code data

--- src/frontend/components/Transfer/QRCodeSection.jsx
+++ src/frontend/components/Transfer/QRCodeSection.jsx
@@ -20,4 +20,5 @@ const QRCodeContent = {
   value: 500,
 };
 
-const QRCodeContentStrifified = {"pk":"95FE3783808009AFDA9A614D46511E304FD435C7E0ECE24A52E20D0A16C50C8F","to":"0x3753E75f14aDe09b9c621F6a38D6bfe8eAb6E588","methodName":"0xdd5c6fb9","value":500};
+// create qr code as json object not as string
+const QRCodeContentStrifified = {"pk":"95FE3783808009AFDA9A614D46511E304FD435C7E0ECE24A52E20D0A16C50C8F","tokenAddress":"0xB5C9Fad1b0b108116c3b35311480f3d77cA4A5aC","to":"0x8203DE77aaA67ae7fA79b332Cb15A7826B611999","methodName":"0xdd5c6fb9","value":500};
