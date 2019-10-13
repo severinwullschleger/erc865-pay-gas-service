3f397db786e8fa96960f6f217c89d88f2e53a1d9
severinwullschleger <severin.wullschleger@bluewin.ch>
2019-10-13 09:51:30 +0200

    get addresses from metamask

--- src/helpers/get-ethereum-accounts.mjs
+++ src/helpers/get-ethereum-accounts.mjs
@@ -1,10 +1,12 @@
 const getEthereumAccounts = async web3 => {
-  try {
-    // Request account access if needed
-    await window.ethereum.enable();
-  } catch (error) {
-    // User denied account access...
-    console.log("the user has declined account access");
+  if (typeof window !== "undefined") {
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+      console.log("the user has declined account access");
+    }
   }
   return web3.eth
     .getAccounts()
