5645c1f6a075e5628843a7654fc00f2b40027f70
Severin Wullschleger <severin.wullschleger@bluewin.ch>
2019-11-10 15:07:01 +0100

    prettier

--- src/helpers/web3Instance.mjs
+++ src/helpers/web3Instance.mjs
@@ -1,15 +1,16 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 import Web3Providers from "../frontend/web3/Web3Providers.mjs";
-import config from "../config.json"
+import config from "../config.json";
 
 let web3;
 export let web3Provider;
 
 const getWeb3Instance = () => {
   if (!web3) {
-    web3 = (typeof window !== "undefined")
-      ? getFrontendWeb3()
-      : new Web3(getBackendProvider());
+    web3 =
+      typeof window !== "undefined"
+        ? getFrontendWeb3()
+        : new Web3(getBackendProvider());
   }
 };
 
@@ -21,53 +22,53 @@ const getFrontendWeb3 = () => {
   }
   // Legacy dapp browsers...
   else if (window.web3) {
-    console.info('MetaMask detected in this browser');
+    console.info("MetaMask detected in this browser");
     web3Provider = Web3Providers.META_MASK;
     return new Web3(window.web3.currentProvider);
   }
   // Non-dapp browsers...
   else {
-    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    console.log(
+      "Non-Ethereum browser detected. You should consider trying MetaMask!"
+    );
     //this service does not need a provider, before: return new Web3('http://localhost:7545');
     web3Provider = Web3Providers.NO_PROVIDER;
     return new Web3();
   }
 };
 
-
 const getBackendProvider = () => {
-
   let protocol = "ws://";
 
-  if (config.ethereumFullNodeAddress === "infura.io/ws")
-    protocol = "wss://";
+  if (config.ethereumFullNodeAddress === "infura.io/ws") protocol = "wss://";
 
   let fullNodeAddress = config.ethereumFullNodeAddress;
 
   if (!fullNodeAddress) {
-    if (config.ethereumNetwork === 'main') {
-      fullNodeAddress = 'infura.io/ws';
-    }
-    else if (config.ethereumNetwork === 'ganache') {
-      fullNodeAddress = '127.0.0.1:7545';
-    }
-    else if (config.ethereumNetwork === 'parity') {
-      fullNodeAddress = '127.0.0.1:8546';
+    if (config.ethereumNetwork === "main") {
+      fullNodeAddress = "infura.io/ws";
+    } else if (config.ethereumNetwork === "ganache") {
+      fullNodeAddress = "127.0.0.1:7545";
+    } else if (config.ethereumNetwork === "parity") {
+      fullNodeAddress = "127.0.0.1:8546";
     }
   }
 
-  web3Provider = new Web3.providers.WebsocketProvider(protocol + fullNodeAddress);
-  
+  web3Provider = new Web3.providers.WebsocketProvider(
+    protocol + fullNodeAddress
+  );
 
-  web3Provider.on('connect', (e) => {
-    console.log('Web3 Provider connected to ' + web3.currentProvider.connection.url);
+  web3Provider.on("connect", e => {
+    console.log(
+      "Web3 Provider connected to " + web3.currentProvider.connection.url
+    );
   });
-  web3Provider.on('error', e => {
-    console.error('Web3 Provider Error', e);
+  web3Provider.on("error", e => {
+    console.error("Web3 Provider Error", e);
     web3 = new Web3(getBackendProvider());
   });
-  web3Provider.on('end', e => {
-    console.error('Web3 Provider Ended', e);
+  web3Provider.on("end", e => {
+    console.error("Web3 Provider Ended", e);
     web3 = new Web3(getBackendProvider());
   });
   return web3Provider;
