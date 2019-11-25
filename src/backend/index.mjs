09e642d725adc99029c8a807fb2e787c85f5f989
Severin Wullschleger <severin.wullschleger@bluewin.ch>
2019-11-25 20:53:03 +0100

    prettier

--- src/backend/index.mjs
+++ src/backend/index.mjs
@@ -1,8 +1,8 @@
-import {isProduction} from "../helpers/isProduction.mjs";
-import express from 'express';
+import { isProduction } from "../helpers/isProduction.mjs";
+import express from "express";
 import dotenv from "dotenv";
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import bodyParser from "body-parser";
+import cors from "cors";
 import router from "./routes/index.mjs";
 import path from "path";
 import mongodb from "mongodb";
@@ -14,7 +14,7 @@ const MongoClient = mongodb.MongoClient;
 
 const startApplication = async () => {
   if (!isProduction()) {
-    dotenv.config();  //import env variables from .env file
+    dotenv.config(); //import env variables from .env file
   }
 
   await MongoClient.connect(process.env.MONGODB_URI, {
@@ -22,8 +22,8 @@ const startApplication = async () => {
     useUnifiedTopology: true
   })
     .then(client => {
-      db = client.db('heroku_g621jvzr');
-      console.log('Connected to database.');
+      db = client.db("heroku_g621jvzr");
+      console.log("Connected to database.");
     })
     .catch(err => {
       console.log(err);
@@ -34,34 +34,36 @@ const startApplication = async () => {
 
   // Serve static files from the React app
   if (isProduction()) {
-    app.use(express.static(path.join(__dirname, '/build')));
+    app.use(express.static(path.join(__dirname, "/build")));
   }
 
   //Parses the text as URL encoded data
-  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   //Parses the text as JSON and exposes the resulting object on req.body.
   app.use(bodyParser.json());
 
-  app.use(cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:8080',
-      'https://erc865-pay-gas-service.herokuapp.com'
-    ]
-  }));
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "https://erc865-pay-gas-service.herokuapp.com"
+      ]
+    })
+  );
 
-  app.use('/api', router);
+  app.use("/api", router);
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
   if (isProduction()) {
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname + '/build/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/build/index.html"));
     });
   }
 
   app.listen(process.env.PORT || 8080, () => {
-    console.log('App started. Date: ' + new Date().toString());
+    console.log("App started. Date: " + new Date().toString());
   });
 };
 
